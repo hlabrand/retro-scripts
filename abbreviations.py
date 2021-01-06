ea61f565bdabd552e8882ceb228d398036dc64f2
hlabrand <hugo@hlabrande.fr>
2021-01-06 11:32:58 -0700

    Abbreviations script: implemented a more rigourous strategy and Matthew Russotto's tiebreaker, and fixed a counting bug.

--- abbreviations.py
+++ abbreviations.py
@@ -26,8 +26,8 @@
 FIRST_FEW_LINES = 0  # With I6, always skip the first 70 lines : 6 lines of header and 64 of your own abbreviations
 LAST_FEW_LINES = 705
 
-# disregard abbreviations that don't save enough bytes
-MIN_SCORE = 10
+# disregard abbreviations that don't save enough units
+MIN_SCORE = 20
 
 # How many do you want?
 #NUMBER_ABBR = 64
@@ -36,7 +36,7 @@ NUMBER_ABBR = 96   # if you want an extra small file, use "string 14 XXXX" (page
 # One-char strings can be 4 unit longs (for instance ";"), so you could save 2 units per occurence; however at the date of writing, Inform refuses to abbreviate strings of length 0 or 1...
 # So starting at 2 is a good idea for now
 MIN_LEN = 2
-MAX_LEN = 17
+MAX_LEN = 60
 
 
 
@@ -67,6 +67,7 @@ for i in range(0,len(lines)):
 
 
 l = []
+        
 
 for n in range(MIN_LEN,MAX_LEN+1):
     dic = {}
@@ -105,11 +106,13 @@ for n in range(MIN_LEN,MAX_LEN+1):
                 units += 4 
             i += 1
         ## number of occurences (-1 since you have to write the abbr somewhere) * units saved (units/2) = total units saved
-        ## 3 units fit in 2 bytes
-        score = int ((p[1]-1)* (units-2)/3 * 2)
-        ## Only add things that save enough bytes (to speed up the search)
+        ## we compute the number of units saved
+        ## score = int ((p[1]-1)* (units-2)/3 * 2)
+        score = (p[1]-1)* (units-2)
+        ## Only add things that save enough units (to speed up the search)
+        ## Add something to say when we last updated the score
         if (score > MIN_SCORE):
-            l += [[p[0], score, units ]]
+            l += [[p[0], score, units, 0 ]]
 
 # find the first abbreviation
 
@@ -122,19 +125,60 @@ if ZAP_OUTPUT == 1:
 
 final_savings = 0
 
+# Python's count thinks "****" has 3 "**", when it only replaces twice; this makes a big difference
+def my_count(text, st):
+    c = 0
+    i = 0
+    while (i <= len(text)-len(st)):
+        # if they have the same first letter...
+        if (text[i] == st[0]):
+            # if it's a match
+            if (text[i:i+len(st)] == st):
+                # increase the count and SKIP TILL THE END OF THE MATCH
+                c += 1
+                i += len(st)
+            else:
+                i +=1
+        else:
+            i+=1
+    return c
+    
+
 steps = 0
 while (len(abbr) < NUMBER_ABBR and len(l) > 0):
     steps += 1
-    # potential winner
-    winner = l[len(l)-1]
-    print("Trying "+str(winner[0])+"...")
-    # let's confirm the score
-    actual_freq = wholetext.count(winner[0])
-    actual_score = int(2*(actual_freq-1)*(winner[2]-2)/3)
-    print ("Let's compare to the score of "+str(l[len(l)-2][0])+", which is", str(l[len(l)-2][1]) )
-    if (actual_score >= l[len(l)-2][1]):
-        print("Found string : '"+str(winner[0])+"' (saves "+str(actual_score)+" bytes)")
-        final_savings += (actual_freq-1)*(winner[2]-2)
+    # determine the leaders
+    leading_score = l[len(l)-1][1]
+    n=1
+    while( l[len(l)-1-n][1] == leading_score ):
+        n += 1
+    print("Found "+str(n)+" leaders with score "+str(leading_score))
+    # see if they all have updated score
+    flag = 1
+    for i in range(1, n+1):
+        if (l[len(l)-i][3] != len(abbr)):
+            l[len(l)-i][3] = len(abbr)
+            actual_freq = my_count(wholetext, l[len(l)-i][0])
+                #actual_score = int(2*(actual_freq-1)*(l[len(l)-i][2]-2)/3)
+            actual_score = (actual_freq-1)*(l[len(l)-i][2]-2)
+            l[len(l)-i][1] = actual_score
+            flag = 0
+    if ( flag == 0 ):
+        l = sorted(l, key=lambda x: x[1])
+        print("The leaders weren't what we thought; let's sort again...")
+    else:
+        # at this point, we have a few candidates with equal actual score
+        # Matthew Russotto's idea for a tiebreaker: take the high frequency
+        #    one (meaning the one with the most weight)
+        max = 1
+        for i in range(2, n+1):
+            if (l[len(l)-max][2] < l[len(l)-i][2]):
+                max = i
+        # we found our abbreviation
+        winner = l[len(l)-max]
+        actual_freq = my_count(wholetext, winner[0])
+        print("Found string : '"+str(winner[0])+"' (saves "+str(winner[1])+" units)")
+        final_savings += winner[1]
         if ZAP_OUTPUT == 1:
             g.write("        .FSTR FSTR?"+str(len(abbr)+1)+",\""+str(winner[0])+"\"        ; "+str(actual_freq)+"x, saved "+str((actual_freq-1)*(winner[2]-2))+"\n")
         # the revised score is still better than the next in line's
@@ -144,18 +188,13 @@ while (len(abbr) < NUMBER_ABBR and len(l) > 0):
         wholetext = wholetext.replace(str(winner[0]), "$")
         # update the array
         lcopy = []
-        for i in range(0, len(l)-1): # skip the last one since it's the abbrev
+        for i in range(0, len(l)):
             # only add to the copy the strings not containing the abbreviated string
             if (str(winner[0]) not in str(l[i][0])):
                 lcopy += [l[i]]
         l = lcopy
         print ("Array updated; now has"+str(len(l))+" entries")
         # no need to sort; the order of the score hasn't been changed
-    else: # calculating the actual score knocked that back a few pegs; sort again
-        l[len(l)-1][1] = actual_score
-        #print("Changed the score of "+str(l[len(l)-1][0])+" to "+str(actual_score))
-        #print("Sorting again...")
-        l = sorted(l, key=lambda x: x[1])
         
         
 if ZAP_OUTPUT == 1:
