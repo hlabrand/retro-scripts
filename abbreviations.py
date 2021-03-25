9649de5adc50b8f43461aa27b2185170a832b3ab
hlabrand <hugo@hlabrande.fr>
2021-03-24 18:19:32 -0600

    Improved the abbreviation-finding script; still not as good as Henrik's and Matthew's though!

--- abbreviations.py
+++ abbreviations.py
@@ -1,11 +1,14 @@
 #   abbreviations.py
 #      by Hugo Labrande
-#      Licence: public domain
-#   Version: 09-Dec-2020
+#   Version: 24-Mar-2021
 
-# This script finds great abbreviations for your Inform game, which helps when space is tight.
+# This script finds very good abbreviations for your Inform game, which helps when space is tight.
 # It finds up to 20% more savings than Inform's "-u" switch ; this is about 4k on a 128k story file.
 # As a bonus, it's just as fast as that switch when you look at abbreviations of length between 2 and 9.
+
+# Other programs doing this exist: https://intfiction.org/t/highly-optimized-abbreviations-computed-efficiently/48753
+# Thank you to Henrik Asman and Matthew Russotto for the discussion. Their programs (for ZIL) find even better abbreviations than this script!
+# You'll find them at https://github.com/heasm66/ZAbbrevMaker and https://gitlab.com/russotto/zilabbrs
 
 # Abbreviation strings still aren't an exact science!
 #   Due to the fact that the number of 5-bit units has to be a multiple of 3 (and as such gets padded), we can only give estimates
@@ -23,11 +26,11 @@
 # In I6, gametext.txt ends with a printout of the dictionary, but the dictionary is never abbreviated
 # so you want to count frequencies for all the file except the "last few lines"
 # tweak this constant to get better estimates
-FIRST_FEW_LINES = 0  # With I6, always skip the first 70 lines : 6 lines of header and 64 of your own abbreviations
-LAST_FEW_LINES = 705
+FIRST_FEW_LINES = 70  # With I6, always skip the first 70 lines : 6 lines of header and 64 of your own abbreviations
+LAST_FEW_LINES = 259
 
 # disregard abbreviations that don't save enough units
-MIN_SCORE = 20
+MIN_SCORE = 15
 
 # How many do you want?
 #NUMBER_ABBR = 64
@@ -37,6 +40,20 @@ NUMBER_ABBR = 96   # if you want an extra small file, use "string 14 XXXX" (page
 # So starting at 2 is a good idea for now
 MIN_LEN = 2
 MAX_LEN = 60
+
+
+# Helper function (weight of a zchar)
+def zchar_weight(c):
+    if (ord(c) == 32):
+        return 1 ## space = char 0
+    elif (ord(c) >= 97 and ord(c) <= 122):
+        return 1 ## A0 alphabet
+    elif (ord(c) >= 65 and ord(c) <= 90):
+        return 2 ## A1 alphabet
+    elif (c in "^0123456789.,!?_#'~/\-:()"):
+        return 2 ## A2 alphabet
+    else:
+        return 4
 
 
 
@@ -64,7 +81,10 @@ lines = lines[FIRST_FEW_LINES:len(lines)-LAST_FEW_LINES]
 wholetext = ""
 for i in range(0,len(lines)):
     wholetext += lines[i] + "\n"
-
+L = len(wholetext)
+wholetext_weight = 0
+for i in range(0, len(wholetext)):
+    wholetext_weight += zchar_weight(wholetext[i])
 
 l = []
         
@@ -80,7 +100,9 @@ for n in range(MIN_LEN,MAX_LEN+1):
                 dic[s] = dic[s] + 1
             else:
                 dic[s] = 1
-                    
+    
+    # First score: naive savings
+    
     ## If you want to use the same formula as inform :
     ##     2*((abbrev_freqs[i]-1)*abbrev_quality[i])/3 with abbrev_quality = len -2
     ## A better one is actually counting the units.
@@ -123,28 +145,40 @@ l = sorted(l, key=lambda x: x[1])
 if ZAP_OUTPUT == 1:
     g = open("mygame_freq.zap", "w")
 
-final_savings = 0
 
-# Python's count thinks "****" has 3 "**", when it only replaces twice; this makes a big difference
-def my_count(text, st):
-    c = 0
-    i = 0
-    while (i <= len(text)-len(st)):
-        # if they have the same first letter...
-        if (text[i] == st[0]):
-            # if it's a match
-            if (text[i:i+len(st)] == st):
-                # increase the count and SKIP TILL THE END OF THE MATCH
-                c += 1
-                i += len(st)
-            else:
-                i +=1
-        else:
-            i+=1
-    return c
+
+
+# given the abbreviation table, what is the optimal way to apply abbreviations for the text?
+# this is given in R.A. Wagner , “Common phrases and minimum-space text storage”, Commun. ACM, 16 (3) (1973), pp. 148-152
+# this gives us better estimates than just replacing in a text and seeing, because of overlap
+def wagner_optimal_parse(text, abblist):
+    # dynamic programming algorithm
+    # f(n) = "least space needed to store the abbreviated form of the characters n...len(text)"
+    N = len(text)
+    f = [0 for i in range(0, N+1)]
+    # you can compute f(n) from all the f(n+k); your goal is f(1)
+    f[N] = 0
+    I=N-1
+    while(I >= 0):
+        # compute f(I) = min( f(I+len(ab))+2, f(I+1)+1) for any matching ab
+        val = f[I+1]+zchar_weight(text[I])
+        for j in range(0, len(abblist)):
+            myabb = abblist[j]
+            if (I+len(myabb) <= len(text) and text[I:I+len(myabb)] == myabb):
+                # the abbreviation j could be used here, but is it a good idea
+                val2 = f[I+len(myabb)]+2
+                if (val2 < val):
+                    val = val2
+        f[I] = val
+        I = I-1
+    #return the minimal text size with optimal parsing
+    return f[0]
     
 
 steps = 0
+old_savings = 0
+old_textsize = wholetext_weight
+
 while (len(abbr) < NUMBER_ABBR and len(l) > 0):
     steps += 1
     # determine the leaders
@@ -158,10 +192,9 @@ while (len(abbr) < NUMBER_ABBR and len(l) > 0):
     for i in range(1, n+1):
         if (l[len(l)-i][3] != len(abbr)):
             l[len(l)-i][3] = len(abbr)
-            actual_freq = my_count(wholetext, l[len(l)-i][0])
-                #actual_score = int(2*(actual_freq-1)*(l[len(l)-i][2]-2)/3)
-            actual_score = (actual_freq-1)*(l[len(l)-i][2]-2)
-            l[len(l)-i][1] = actual_score
+            # compute how small the text representation would be with that abbreviation added to the set
+            # store the difference "without this abbreviation minus with this abbreviation", both computed with optimal parse
+            l[len(l)-i][1] = old_textsize-wagner_optimal_parse(wholetext, abbr+[l[len(l)-i][0]])
             flag = 0
     if ( flag == 0 ):
         l = sorted(l, key=lambda x: x[1])
@@ -176,16 +209,15 @@ while (len(abbr) < NUMBER_ABBR and len(l) > 0):
                 max = i
         # we found our abbreviation
         winner = l[len(l)-max]
-        actual_freq = my_count(wholetext, winner[0])
-        print("Found string : '"+str(winner[0])+"' (saves "+str(winner[1])+" units)")
-        final_savings += winner[1]
+        print("Found string : '"+str(winner[0])+"' (units saved: "+str(winner[1])+" units)")
+        # the abbreviated text size is decreased by the savings
+        old_textsize = old_textsize - winner[1]
         if ZAP_OUTPUT == 1:
-            g.write("        .FSTR FSTR?"+str(len(abbr)+1)+",\""+str(winner[0])+"\"        ; "+str(actual_freq)+"x, saved "+str((actual_freq-1)*(winner[2]-2))+"\n")
+            #g.write("        .FSTR FSTR?"+str(len(abbr)+1)+",\""+str(winner[0])+"\"        ; "+str(actual_freq)+"x, saved "+str((actual_freq-1)*(winner[2]-2))+"\n")
+            g.write("        .FSTR FSTR?"+str(len(abbr)+1)+",\""+str(winner[0])+"\"        ; "+"\n")
         # the revised score is still better than the next in line's
         abbr = abbr + [str(winner[0])]
         print("Determining abbreviation "+str(len(abbr)+1)+"...")
-        # update the text
-        wholetext = wholetext.replace(str(winner[0]), "$")
         # update the array
         lcopy = []
         for i in range(0, len(l)):
@@ -194,15 +226,16 @@ while (len(abbr) < NUMBER_ABBR and len(l) > 0):
                 lcopy += [l[i]]
         l = lcopy
         print ("Array updated; now has"+str(len(l))+" entries")
-        # no need to sort; the order of the score hasn't been changed
+        # no need to sort; the order of the score hasn't changed
         
         
 if ZAP_OUTPUT == 1:
     endoffile="WORDS::\n        FSTR?1\n        FSTR?2\n        FSTR?3\n        FSTR?4\n        FSTR?5\n        FSTR?6\n        FSTR?7\n        FSTR?8\n        FSTR?9\n        FSTR?10\n        FSTR?11\n        FSTR?12\n        FSTR?13\n        FSTR?14\n        FSTR?15\n        FSTR?16\n        FSTR?17\n        FSTR?18\n        FSTR?19\n        FSTR?20\n        FSTR?21\n        FSTR?22\n        FSTR?23\n        FSTR?24\n        FSTR?25\n        FSTR?26\n        FSTR?27\n        FSTR?28\n        FSTR?29\n        FSTR?30\n        FSTR?31\n        FSTR?32\n        FSTR?33\n        FSTR?34\n        FSTR?35\n        FSTR?36\n        FSTR?37\n        FSTR?38\n        FSTR?39\n        FSTR?40\n        FSTR?41\n        FSTR?42\n        FSTR?43\n        FSTR?44\n        FSTR?45\n        FSTR?46\n        FSTR?47\n        FSTR?48\n        FSTR?49\n        FSTR?50\n        FSTR?51\n        FSTR?52\n        FSTR?53\n        FSTR?54\n        FSTR?55\n        FSTR?56\n        FSTR?57\n        FSTR?58\n        FSTR?59\n        FSTR?60\n        FSTR?61\n        FSTR?62\n        FSTR?63\n        FSTR?64\n        FSTR?65\n        FSTR?66\n        FSTR?67\n        FSTR?68\n        FSTR?69\n        FSTR?70\n        FSTR?71\n        FSTR?72\n        FSTR?73\n        FSTR?74\n        FSTR?75\n        FSTR?76\n        FSTR?77\n        FSTR?78\n        FSTR?79\n        FSTR?80\n        FSTR?81\n        FSTR?82\n        FSTR?83\n        FSTR?84\n        FSTR?85\n        FSTR?86\n        FSTR?87\n        FSTR?88\n        FSTR?89\n        FSTR?90\n        FSTR?91\n        FSTR?92\n        FSTR?93\n        FSTR?94\n        FSTR?95\n        FSTR?96\n\n        .ENDI"
     g.write(endoffile)
     g.close()
-            
-print("Found "+str(NUMBER_ABBR)+" abbreviations in"+str(steps)+"steps; they saved "+str(final_savings)+" units (~"+str(2*int(final_savings/3))+" bytes)")
+
+final_savings = wholetext_weight - old_textsize
+print("Found "+str(NUMBER_ABBR)+" abbreviations in "+str(steps)+" steps; they saved "+str(final_savings)+" units (~"+str(2*int(final_savings/3))+" bytes)")
 
 s = "Abbreviate "
 for i in range(0,64):
